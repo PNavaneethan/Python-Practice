11b6f40524f1af212acc329b85ad91ab3ea72a02
PNavaneethan <98250103+PNavaneethan@users.noreply.github.com>
2026-07-17 23:45:46 +0530

    Create Program5.py

---
+++ Programs/Program5.py
@@ -0,0 +1,9 @@
+//Find the largest of two numbers.
+
+a = int(input("Enter the First Number: "))
+b = int(input("Enter the Second Number: "))
+
+if a > b:
+  print("The Largest Number is:", a)
+else:
+  print("The Largest Number is:", b)
